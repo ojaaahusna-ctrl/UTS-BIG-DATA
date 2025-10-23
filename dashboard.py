aba57792e6bf60b037c5f1d3963e876d11f50b20
ojaaahusna-ctrl <ojaaahusna@gmail.com>
2025-10-23 20:51:06 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -151,7 +151,7 @@ div[data-baseweb="input"], div[data-baseweb="textarea"] {
 @st.cache_resource(show_spinner="📦 Memuat model YOLO...")
 def load_yolo_model():
     try:
-        return YOLO("model/best.pt")
+        return YOLO("model/Raudhatul Husna_laporan4.pt")
     except Exception as e:
         st.error(f"❌ Gagal memuat model YOLO: {e}", icon="🔥")
         return None
@@ -159,7 +159,7 @@ def load_yolo_model():
 @st.cache_resource(show_spinner="📦 Memuat model CNN...")
 def load_cnn_model():
     try:
-        return tf.keras.models.load_model("compressed.h5", compile=False)
+        return tf.keras.models.load_model("Raudhatul Husna_laporan2.h5", compile=False)
     except Exception as e:
         st.error(f"❌ Gagal memuat model CNN: {e}", icon="🔥")
         return None
